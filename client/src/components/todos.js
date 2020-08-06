61fa3f8ccbf48a92bd887b5ef3f72bb450a7f0b7
Mayowa Yakub <mayoa.net@gmail.com>
2020-08-06 21:16:11 +0100

    client basic setup

---
+++ client/src/components/todos.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Todos = ({ todos }) => (
+  <div className="todos">
+    <div className="todo">Hey yo</div>
+    <div className="todo">Hey yo</div>
+    <div className="todo">Hey yo</div>
+    <div className="todo">Hey yo</div>
+  </div>
+)
+
+export default Todos
