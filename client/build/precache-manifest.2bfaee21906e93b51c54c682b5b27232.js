c6d15d5f12820405771b10c9fc95a81740343efc
Mayowa Yakub <mayoa.net@gmail.com>
2020-08-06 18:50:53 +0100

    Client build

---
+++ client/build/precache-manifest.2bfaee21906e93b51c54c682b5b27232.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3813aee677cebca3ef787a3747fce88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90709dcef745788c3c1f",
+    "url": "/static/css/main.20788f62.chunk.css"
+  },
+  {
+    "revision": "6e32f2e52f668f87046a",
+    "url": "/static/js/2.e8ce996f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e8ce996f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90709dcef745788c3c1f",
+    "url": "/static/js/main.8596d723.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "8dad80289a1ece380591ad143b616747",
+    "url": "/static/media/logo.8dad8028.svg"
+  }
+]);
